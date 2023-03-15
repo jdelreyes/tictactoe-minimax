67284271e51d42c8783b1c616e9f694631750b22
Jerome Delos Reyes <delosreyes.jerome@outlook.com>
2023-03-15 12:04:40 -0400

    able to recognize if someone won

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 // TODO: use Random class to generate random integers for accessing the 2 dimensional array.
 //      This helps us make an easy AI
+// TODO: the code messy, fix later
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         String aiType, choice;
-        char playerSymbol = ' ', computerSymbol = ' ';
+        char playerSymbol = ' ', aiSymbol = ' ';
         int move;
 
         System.out.print("Please enter your name: ");
@@ -17,28 +18,34 @@ public class Main {
 
         do {
             System.out.println("-------------------------");
-            System.out.println("Symbols: \nenter 0 for symbol:\tO\nenter 1 for symbol:\tX");
+            System.out.println("Symbols: \nenter 1 for symbol:\tO\nenter 2 for symbol:\tX");
             System.out.print("Please choose a symbol: ");
             String symbol = scanner.nextLine();
-            if (symbol.equals("0")) playerSymbol = 'O';
-            if (symbol.equals("1")) playerSymbol = 'X';
+            if (symbol.equals("1")) {
+                playerSymbol = 'O';
+                aiSymbol = 'X';
+            }
+            if (symbol.equals("2")) {
+                playerSymbol = 'X';
+                aiSymbol = 'O';
+            }
         } while (playerSymbol != 'O' && playerSymbol != 'X');
 
         do {
             System.out.println("-------------------------");
-            System.out.println("AI Type: \nenter 0 for:\tWeak AI\nenter 1 for:\tIntelligent AI");
+            System.out.println("AI Type: \nenter 1 for:\tWeak AI\nenter 2 for:\tIntelligent AI");
             System.out.print("Please choose an AI: ");
             aiType = scanner.nextLine();
-        } while (!aiType.equals("0") && !aiType.equals("1"));
+        } while (!aiType.equals("1") && !aiType.equals("2"));
 
         do {
             System.out.println("-------------------------");
-            System.out.println("If you wish to play first, enter 0. Otherwise, enter 1");
+            System.out.println("If you wish to play first, enter 1. Otherwise, enter 2");
             System.out.print("Please enter your choice: ");
             choice = scanner.nextLine();
-        } while(!choice.equals("0") && !choice.equals("1"));
+        } while(!choice.equals("1") && !choice.equals("2"));
 
-        ticTacToe.createPlayer(playerName, playerSymbol);
+        ticTacToe.createGame(playerName, playerSymbol, aiSymbol);
 
         System.out.println(ticTacToe.printBoard());
 
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,85 +1,136 @@
+import java.util.Random;
+
 public class TicTacToe {
     private char[][] board;
     private Player player;
+    private char aiSymbol;
 
     // creating a tic-tac-toe board
     public TicTacToe() {
-        // delete this and symbols later. for printing purposes [0][0] to [2][2]
         board = new char[][] {
-                {' ', ' ', ' '},
-                {' ', ' ', ' '},
-                {' ', ' ', ' '}
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
         };
         player = new Player();
     }
 
-    public void createPlayer(String name, char playerSymbol) {
-        player.setName(name);
+    // TODO: delete later
+    public void createGame(String playerName, char playerSymbol, char aiSymbol) {
+        player.setName(playerName);
+        player.setPlayerSymbol(playerSymbol);
+        this.aiSymbol = aiSymbol;
+    }
+
+    public void createGame(String playerName, char playerSymbol,
+                           String aiType, char aiSymbol) {
+        player.setName(playerName);
         player.setPlayerSymbol(playerSymbol);
     }
 
+    // IDEA: this could be private and act as a helper function
     public boolean makeMove(int move) {
-        if (isFull()) return false;
         if (move > 9 || move < 0) return true;
         switch (move) {
-            case 1:
-                if (board[0][0] != ' ') return true;
+            case 1 -> {
+                if (board[0][0] == 'X' || board[0][0] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[0][0] = player.getPlayerSymbol();
-                break;
-            case 2:
-                if (board[0][1] != ' ') return true;
+            }
+            case 2 -> {
+                if (board[0][1] == 'X' || board[0][1] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[0][1] = player.getPlayerSymbol();
-                break;
-            case 3:
-                if (board[0][2] != ' ') return true;
+            }
+            case 3 -> {
+                if (board[0][2] == 'X' || board[0][2] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[0][2] = player.getPlayerSymbol();
-                break;
-            case 4:
-                if (board[1][0] != ' ') return true;
+            }
+            case 4 -> {
+                if (board[1][0] == 'X' || board[1][0] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[1][0] = player.getPlayerSymbol();
-                break;
-            case 5:
-                if (board[1][1] != ' ') return true;
+            }
+            case 5 -> {
+                if (board[1][1] == 'X' || board[1][1] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[1][1] = player.getPlayerSymbol();
-                break;
-            case 6:
-                if (board[1][2] != ' ') return true;
+            }
+            case 6 -> {
+                if (board[1][2] == 'X' || board[1][2] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[1][2] = player.getPlayerSymbol();
-                break;
-            case 7:
-                if (board[2][0] != ' ') return true;
+            }
+            case 7 -> {
+                if (board[2][0] == 'X' || board[2][0] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[2][0] = player.getPlayerSymbol();
-                break;
-            case 8:
-                if (board[2][1] != ' ') return true;
+            }
+            case 8 -> {
+                if (board[2][1] == 'X' || board[2][1] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[2][1] = player.getPlayerSymbol();
-                break;
-            case 9:
-                if (board[2][2] != ' ') return true;
+            }
+            case 9 -> {
+                if (board[2][2] == 'X' || board[2][2] == 'O') {
+                    System.out.println("The spot is already filled in!");
+                    return true;
+                }
                 board[2][2] = player.getPlayerSymbol();
-                break;
+            }
         }
         System.out.println(printBoard());
-        return true;
+        if (someoneWon()) {
+            System.out.println("someone won(change this later)");
+            return false;
+        }
+        return isFull();
     }
 
-    // the 2 functions below add moves to the board
-    // can be removed later
-    public boolean moveX(int column, int row) {
-        return false;
-    }
+    // AI
+    public boolean weakAIMove() {
+        Random random = new Random();
+        int column = random.nextInt(9) + 1;
+        int row = random.nextInt(9) + 1;
 
-    public boolean moveO(int column, int row) {
         return false;
     }
 
     public boolean isFull() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == ' ') return false;
+                if (board[i][j] != 'X' && board[i][j] != 'O') return false;
             }
         }
         return true;
+    }
+
+    private boolean someoneWon() {
+        return (board[0][0] == board [0][1] && board[0][0] == board [0][2]) ||
+                (board[0][0] == board [1][1] && board[0][0] == board [2][2]) ||
+                (board[0][0] == board [1][0] && board[0][0] == board [2][0]) ||
+                (board[2][0] == board [2][1] && board[2][0] == board [2][2]) ||
+                (board[2][0] == board [1][1] && board[2][0] == board [0][2]) ||
+                (board[0][2] == board [1][2] && board[0][2] == board [2][2]) ||
+                (board[0][1] == board [1][1] && board[0][1] == board [2][1]) ||
+                (board[1][0] == board [1][1] && board[1][0] == board [1][2]);
     }
 
     public String printBoard() {
