77e84a43dc446ced93d48cf94bd2d3703dd28950
Jerome Delos Reyes <delosreyes.jerome@outlook.com>
2023-03-15 23:03:34 -0400

    i got all working instead of the intelligent AI. however, we need to clean the code

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 import java.util.Scanner;
 
-// TODO: use Random class to generate random integers for accessing the 2 dimensional array.
-//      This helps us make an easy AI
-// TODO: the code messy, fix later
-// TODO: how do i better organize this?
-// TODO: we need some sort of interpreter
-
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
         String aiMode, choice;
@@ -32,6 +27,7 @@ public class Main {
             }
         } while (playerSymbol != 'O' && playerSymbol != 'X');
 
+        // TODO: not functional yet, make this functional
         do {
             System.out.println("-------------------------");
             System.out.println("AI Type: \nenter 1 for:\tWeak AI\nenter 2 for:\tIntelligent AI");
@@ -53,11 +49,21 @@ public class Main {
 
         int x;
 
-        do {
-            System.out.print("Please make a move: ");
-            x = scanner.nextInt();
-
-        } while (ticTacToe.playerMove(x));
-
+        while (true) {
+            if (choice.equals("1")) {
+                System.out.print("Please make a move: ");
+                x = scanner.nextInt();
+                if (!ticTacToe.playerMove(x)) break;
+                System.out.println("AI MOVE");
+                if (!ticTacToe.weakAIMove()) break;
+            }
+            if (choice.equals("2")) {
+                System.out.println("AI MOVE");
+                if (!ticTacToe.weakAIMove()) break;
+                System.out.print("Please make a move: ");
+                x = scanner.nextInt();
+                if (!ticTacToe.playerMove(x)) break;
+            }
+        }
     }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,13 +6,13 @@ public class TicTacToe {
     private char aiSymbol;
     private String aiMode;
 
-    // creating a tic-tac-toe board
     public TicTacToe(String playerName, char playerSymbol, char aiSymbol, String aiMode) {
-        board = new char[][] {
+        board = new char[][]{
                 {'1', '2', '3'},
                 {'4', '5', '6'},
                 {'7', '8', '9'}
         };
+
         player = new Player();
 
         player.setName(playerName);
@@ -20,84 +20,76 @@ public class TicTacToe {
         this.aiSymbol = aiSymbol;
         this.aiMode = aiMode;
     }
-    public boolean isAvailable(int move) {
-        int[] coordinate = coordinates(move);
-        if (board[coordinate[0]][coordinate[1]] == 'X' || board[coordinate[0]][coordinate[1]] == 'O')
-            return false;
-        return true;
-    }
 
-    // AI
     public boolean weakAIMove() {
         Random random = new Random();
-        int column = random.nextInt(9) + 1;
-        int row = random.nextInt(9) + 1;
 
+        int move = random.nextInt(9) + 1;
+        int moveCondition = makeMove(move, aiSymbol);
 
+        while (moveCondition == 1 || moveCondition == 2) {
+            move = random.nextInt(9) + 1;
+            moveCondition = makeMove(move, aiSymbol);
+        }
 
-        return false;
+        if (moveCondition == 3) {
+            System.out.println("The AI won(change later)");
+            return false;
+        }
+        if (moveCondition == 4) {
+            System.out.println("DRAW! (change later)");
+            return false;
+        }
+
+        return moveCondition == 5;
     }
 
     public boolean playerMove(int move) {
-        if (move > 9 || move < 0) {
-            System.out.println("Please enter a valid move");
-            return true;
-        }
-        if (!isAvailable(move)) {
-            System.out.println("Please enter a move to unoccupied spot");
-            return true;
-        }
-        makeMove(move, player.getPlayerSymbol());
-        if (winning()) {
-            System.out.println("someone won (change later)");
+
+        int moveCondition = makeMove(move, player.getPlayerSymbol());
+
+        if (moveCondition == 1 || moveCondition == 2) {
+            System.out.println("Please enter a valid number");
             return false;
         }
-        if (isFull()) return false;
+        if (moveCondition == 3) {
+            System.out.println("The Player won(change later)");
+            return false;
+        }
+        if (moveCondition == 4) {
+            System.out.println("DRAW! (change later)");
+            return false;
+        }
+        // if its valid
+        return moveCondition == 5;
+    }
+
+    private int makeMove(int move, char symbol) {
+        if (!isAvailable(move)) return 1;
+        if (!isValidMove(move)) return 2;
+
+        int[] coordinates = getCoordinates(move);
+        board[coordinates[0]][coordinates[1]] = symbol;
+        System.out.println(printBoard());
+
+        if (isWinning()) return 3;
+        if (isBoardFull()) return 4;
+
+        return 5;
+    }
+
+    private boolean isValidMove(int move) {
+        return move <= 9 && move >= 1;
+    }
+
+    private boolean isAvailable(int move) {
+        int[] coordinates = getCoordinates(move);
+        if (board[coordinates[0]][coordinates[1]] == 'X' || board[coordinates[0]][coordinates[1]] == 'O')
+            return false;
         return true;
     }
 
-    private int[] coordinates(int move) {
-        switch (move) {
-            case 1 -> {
-                return new int[] {0, 0};
-            }
-            case 2 -> {
-                return new int[] {0, 1};
-            }
-            case 3 -> {
-                return new int[] {0, 2};
-            }
-            case 4 -> {
-                return new int[] {1, 0};
-            }
-            case 5 -> {
-                return new int[] {1, 1};
-            }
-            case 6 -> {
-                return new int[] {1, 2};
-            }
-            case 7 -> {
-                return new int[] {2, 0};
-            }
-            case 8 -> {
-                return new int[] {2, 1};
-            }
-            case 9 -> {
-                return new int[] {2, 2};
-            }
-            default -> {
-                return new int[] {-1, -1};
-            }
-        }
-    }
-
-    public void makeMove(int move, char symbol) {
-        int[] coordinate = coordinates(move);
-        board[coordinate[0]][coordinate[1]] = symbol;
-        System.out.println(printBoard());
-    }
-
-    private boolean isFull() {
+    private boolean isBoardFull() {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 if (board[i][j] != 'X' && board[i][j] != 'O') return false;
@@ -106,27 +98,60 @@ public class TicTacToe {
         return true;
     }
 
-    private boolean winning() {
-        return (board[0][0] == board [0][1] && board[0][0] == board [0][2]) ||
-                (board[0][0] == board [1][1] && board[0][0] == board [2][2]) ||
-                (board[0][0] == board [1][0] && board[0][0] == board [2][0]) ||
-                (board[2][0] == board [2][1] && board[2][0] == board [2][2]) ||
-                (board[2][0] == board [1][1] && board[2][0] == board [0][2]) ||
-                (board[0][2] == board [1][2] && board[0][2] == board [2][2]) ||
-                (board[0][1] == board [1][1] && board[0][1] == board [2][1]) ||
-                (board[1][0] == board [1][1] && board[1][0] == board [1][2]);
+    private boolean isWinning() {
+        return (board[0][0] == board[0][1] && board[0][0] == board[0][2]) ||
+                (board[0][0] == board[1][1] && board[0][0] == board[2][2]) ||
+                (board[0][0] == board[1][0] && board[0][0] == board[2][0]) ||
+                (board[2][0] == board[2][1] && board[2][0] == board[2][2]) ||
+                (board[2][0] == board[1][1] && board[2][0] == board[0][2]) ||
+                (board[0][2] == board[1][2] && board[0][2] == board[2][2]) ||
+                (board[0][1] == board[1][1] && board[0][1] == board[2][1]) ||
+                (board[1][0] == board[1][1] && board[1][0] == board[1][2]);
+    }
+
+    private int[] getCoordinates(int move) {
+        switch (move) {
+            case 1 -> {
+                return new int[]{0, 0};
+            }
+            case 2 -> {
+                return new int[]{0, 1};
+            }
+            case 3 -> {
+                return new int[]{0, 2};
+            }
+            case 4 -> {
+                return new int[]{1, 0};
+            }
+            case 5 -> {
+                return new int[]{1, 1};
+            }
+            case 6 -> {
+                return new int[]{1, 2};
+            }
+            case 7 -> {
+                return new int[]{2, 0};
+            }
+            case 8 -> {
+                return new int[]{2, 1};
+            }
+            case 9 -> {
+                return new int[]{2, 2};
+            }
+        }
+        return new int[0];
     }
 
     public String printBoard() {
         String output = "";
         for (int i = 0; i < 3; i++) {
-            output +=  " ------------- \n";
+            output += " +---+---+---+ \n";
             for (int j = 0; j < 3; j++) {
                 output += " | " + this.board[i][j];
             }
             output += " |\n";
         }
-        output += " ------------- ";
+        output += " +---+---+---+ ";
         return output;
     }
 }
